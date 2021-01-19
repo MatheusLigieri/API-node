074f89b1f6d6259f46eb8f6cfad838f8f639ce3e
Matheus Ligieri <matheusligieri@gmail.com>
2021-01-19 01:21:38 -0300

    feature/metodo post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,10 @@
+const Atendimento = require('../models/atendimentos');
+
 module.exports = app => {
     app.get('/atendimento', (req, res) => res.send('vc está na rota de atendimento'))
-    app.post('/atendimento', (req, res) => res.send('vc está na rota de atendimento'))
+    app.post('/atendimento', (req, res) => {
+        const atendimento = req.body;
+
+        Atendimento.adiciona(atendimento)
+    })
 }
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const customExpress = require('./config/customExpress')
-
-const app = customExpress()
-
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas')
+conexao.connect(erro => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        Tabelas.init(conexao)
+        const app = customExpress()
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
